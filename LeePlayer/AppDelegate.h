ad1363f396813d0387374b330e5b77101fe12e6a
liping <li.ping@moxiangroup.com>
2017-10-10 17:13:55 +0800

    Initial Commit

---
+++ LeePlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LeePlayer
+//
+//  Created by moxian on 2017/10/10.
+//  Copyright © 2017年 moxian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LeePlayer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LeePlayer
+//
+//  Created by moxian on 2017/10/10.
+//  Copyright © 2017年 moxian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
